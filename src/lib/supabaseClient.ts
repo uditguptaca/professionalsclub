d77b424490b06726a3e42e4afca6f66a246353df
LQBhavika <bhavikalakhanpal9@gmail.com>
2026-04-24 13:58:53 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn('Supabase URL or Anon Key is missing. Check your .env.local file.');
-}
+const supabaseUrl = 'https://dxfxpjzjmxglfkcmrovk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdWJhc2UiLCJyZWYiOiJkeGZ4cGp6am14Z2xma2Ntcm92ayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzc2NzAxNjM3LCJleHAiOjIwOTIyNzc2Mzd9.L_3gezmRk0ZT9wscbcK2ja0HccKLx2uw1qJKOj6fjE4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
